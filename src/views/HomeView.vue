37b850d4677301569e6b0f326771c894805764b5
COGNON <103960857+COGNON@users.noreply.github.com>
2023-06-08 19:52:01 -0400

    Added basic timer. Working start & stop buttons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,40 @@
 import { ref } from 'vue';
 import TInput from '@/components/TInput.vue';
 import TBtn from '@/components/TBtn.vue';
+import TTimer from '@/components/TTimer.vue';
 
 const timeInput = ref(5);
+const displayActualNumber = ref(0); // time in actual seconds
+
+let timerEnd: number, timerCount: number;
+function onStart() {
+  if (!timeInput.value) return;
+
+  displayActualNumber.value = timeInput.value * 60;
+
+  timerEnd = setTimeout(onTimerEnd, timeInput.value * 60 * 1000);
+  timerCount = setInterval(onTimerCount, 1000);
+}
+
+function onTimerEnd() {
+  displayActualNumber.value = 0;
+  clearTimeout(timerEnd);
+  clearInterval(timerCount);
+}
+
+function onTimerCount() {
+  displayActualNumber.value -= 1;
+}
 </script>
 
 <template>
-  <div class="row items-center t-gutter-x-sm">
-    <t-input v-model="timeInput" label="Time" />
-    <t-btn label="Start" />
+  <div class="t-input-wrapper row items-center t-gutter-x-sm">
+    <t-input v-model.number="timeInput" label="Time" @keyup.enter="onStart()" />
+    <t-btn label="Start" @click.stop="onStart()" />
+    <t-btn label="Stop" @click.stop="onTimerEnd()" />
   </div>
+
+  <template v-if="displayActualNumber">
+    <t-timer v-model="displayActualNumber" />
+  </template>
 </template>
